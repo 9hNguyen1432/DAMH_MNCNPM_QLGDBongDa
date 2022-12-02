a34b44bc26cc6330fd761ce388520614074a5ff8
heoquaycutekhung <chinhnguyen140302@gmail.com>
2022-12-02 16:04:54 +0700

    commit layout v2

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/static',express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 
 require("./config/hbs")(app);
