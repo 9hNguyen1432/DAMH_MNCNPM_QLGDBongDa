0ee59d54c81c6fcc9a2d215de2e4f4ea702f1b80
Vo Tu Trinh <votutrinh2002@gmail.com>
2022-12-03 15:31:40 +0700

    abc

--- index.js
+++ index.js
@@ -1,19 +1,1 @@
-const express = require("express");
-const handlebars = require("express-handlebars");
 
-
-const app = express();
-const port = 3000;
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-require("./config/hbs")(app);
-
-app.get('/', (req,res)=>{
-    res.send("<h1>Hello World</h1>")
-})
-// app.use('/user', require("./routers/user.r"))
-
-app.listen (port, () =>
-            console.log(`Example app listening at http://localhost:${port}`));
