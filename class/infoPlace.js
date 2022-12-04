630a7e4fb7a327aa59e675b10e8d7449b6a09ab0
quytunghcmus <tungluu210201@gmail.com>
2022-12-04 16:32:59 +0700

    qtung thêm class

---
+++ class/infoPlace.js
@@ -0,0 +1,9 @@
+class infoPlace
+{
+    constructor(placeName, addr)
+    {
+        this.placeName=placeName
+        this.addr=addr
+    }
+    
+}
---
+++ class/resultOfMatch.js
@@ -0,0 +1,12 @@
+import {resultOfTeam} from './resultOfTeam.js'
+class resultOfMatch{
+    constructor(team1,team2)//team1,team2 : resultOfTeam
+    {
+        this.team1=team1
+        this.team2=team2    
+    }
+    getter()
+    {
+        return this
+    }
+}
