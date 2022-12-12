89bb99de2f1b91568011f4b36d315b094b33a7c8
20120597 <toainguyenvan20102002@gmail.com>
2022-12-12 10:07:18 +0700

    cmt

--- models/club.js
+++ models/club.js
@@ -1,28 +1,44 @@
 const firebase = require('../config/database/config.js');
 const database = firebase.database();
+<<<<<<< Updated upstream
 // const storageRef = firebase.storage().ref();
 
+=======
+const storage = firebase.storage();
+const storageRef = firebase.storage().ref();
+>>>>>>> Stashed changes
 
-class Club
-{
-    constructor(id,logo,name, stadium,coach,captain,score,numberMatch,win,draw,lost,description)
-    {
-        this.id =id;
-        this.logo = logo;
-        this.name=name
-        this.stadium=stadium
-        this.coach = coach;
-        this.captain = captain;
-        this.score = score;
+class Club {
+  constructor(
+    id,
+    logo,
+    name,
+    stadium,
+    coach,
+    captain,
+    score,
+    numberMatch,
+    win,
+    draw,
+    lost,
+    description
+  ) {
+    this.id = id;
+    this.logo = logo;
+    this.name = name;
+    this.stadium = stadium;
+    this.coach = coach;
+    this.captain = captain;
+    this.score = score;
 
-        this.numberMatch = numberMatch ;
-        this.win = win;
-        this.draw = draw;
-        this.lost = lost;
-        this.description = description;
+    this.numberMatch = numberMatch;
+    this.win = win;
+    this.draw = draw;
+    this.lost = lost;
+    this.description = description;
 
-        //Khởi tạo club ban đầu với score = 1; win = 0; draw = 1; lost = 0
-    }
+    //Khởi tạo club ban đầu với score = 1; win = 0; draw = 1; lost = 0
+  }
 
     async addClub(club){
         await this.setID();
@@ -47,6 +63,7 @@ class Club
       });
     });
     return club;
+<<<<<<< Updated upstream
     }
     async getAllClub(){
         var club = [];
@@ -57,6 +74,45 @@ class Club
     });
         return club;
     }
+=======
+  }
+
+  async uploadImage(file) {
+    const ref = firebase.storage().ref();
+
+    const name = new Date() + '-' + file.filename;
+
+    const metadata = {
+      contentType: file.type
+    }
+
+    const task = ref.child(name).put(file, metadata);
+
+    task.then((snapshot) => {
+      snapshot.ref.getDownloadURL()
+      
+    }).then( url => {
+      console.log(url)
+      alert("Image Upload Success")
+      
+    })
+  }
+
+  async uploadLogo(uri) {
+    var d = new Date();
+    var urlImage;
+    var ref = storageRef.child(d.getTime().toString() + ".png");
+    console.log(ref);
+    await ref.put(uri);
+    // await ref.put(uri).then((snapshot) => {
+    //   var temp = ref
+    //     .getDownloadURL()
+    //     .then((url) => {
+    //       urlImage += url.toString();
+    //     })
+    //     .catch((error) => {});
+    // });
+>>>>>>> Stashed changes
 
     
     async setLogo(uri){
@@ -66,4 +122,8 @@ class Club
     }
 }
 
+<<<<<<< Updated upstream
 module.exports = new Club
+=======
+module.exports = new Club();
+>>>>>>> Stashed changes
