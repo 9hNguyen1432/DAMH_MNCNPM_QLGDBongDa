51798bc221d2a833c8e4198e306d6ddb9587fd02
20120597 <toainguyenvan20102002@gmail.com>
2022-12-13 11:41:41 +0700

    toai lam xong lich thi dau va ket qua

--- config/hbs.js
+++ config/hbs.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
 
-
-module.exports = app =>{
-    app.engine('hbs', handlebars.engine({
-    extname: '.hbs'
-}));
-app.set('view engine', 'hbs');
-}
+module.exports = (app) => {
+  app.engine(
+    "hbs",
+    handlebars.engine({
+      extname: ".hbs",
+      helpers:{
+        ifNotEqualZero: function(value,options) {
+            if(value > 0){
+                return options.fn(this);
+            }
+        }
+      }
+    })
+  );
+  app.set("view engine", "hbs");
+};
--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -8,15 +8,25 @@ const Match = require('../models/Match')
 class scheduleController{
 
     async index(req,res){
+        const allDate = await Match.getAllDate();
         const date = await Match.getDateNotFinish();
-        var Matchs =[]
-        await Promise.all(date.map(async d => {
-            var match = await Match.getMatchByDate(d)
-            Matchs.push(match)
+        var AllMatchs = []
+        var Matchs = []
+
+        await Promise.all(allDate.map(async d => {
+            let match = await Match.getMatchByDate(d);
+            AllMatchs.push(match);
         }))
 
-        
-        res.render('danhsachtrandau',{date,Matchs})
+        await Promise.all(date.map(async d => {
+            var match = await Match.getMatchByDate(d);
+            Matchs.push(match);
+        }))
+        const match1 = Matchs[0];
+        const match2 = Matchs[1];
+        const match3 = Matchs[2];
+
+        res.render('danhsachtrandau',{AllMatchs ,date1: date[0], date2: date[1], date3: date[2] ,match1,match2, match3})
     }
     
 }
