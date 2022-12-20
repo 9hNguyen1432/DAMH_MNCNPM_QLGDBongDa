c0361ab119ab0c52e3893aad59b4f1318fee363b
quytunghcmus <tungluu210201@gmail.com>
2022-12-20 20:40:48 +0700

    tung sua xep bang xep hang

--- controllers/RankController.js
+++ controllers/RankController.js
@@ -5,34 +5,31 @@ class RankController{
     async index(req,res){
 
         var CLB = await Club.getAllClub();
-
+        for(var i = 0; i < CLB.length;i++)
+        {
+            console.log(CLB[i])
+        }
         const Rule = await Rules.getRulesFromDataBase();
 
         var priorityRank = Rule.priorityToRank;
 
-         CLB = await CLB.sort(async (a,b)=>{
-            
+        CLB = CLB.sort(function(a,b){
             const _score = a.score - b.score;
-            const _goalDelta = b.goalDelta - a.goalDelta;
-            const _totalGoal = b.totalGoal - a.totalGoal;
-            const _facing = await Match.getFacingOf2Club(a.name,b.name);
+            const _goalDelta = a.goalDelta - b.goalDelta;
+            const _totalGoal = a.totalGoal - b.totalGoal;
+            const _facing = Match.getFacingOf2Club(a.name,b.name);
 
-            const ranks = {score:_score, goalDelta:_goalDelta,totalGoal:_totalGoal,facing:_facing[1]-_facing[0]}
+            const ranks = {score:_score, goalDelta:_goalDelta,totalGoal:_totalGoal,facing:_facing[0]-_facing[1]}
             
             const  priority = [ranks[priorityRank["p1"]],ranks[priorityRank["p2"]],ranks[priorityRank["p3"]],ranks[priorityRank["p4"]]]
-
-            for(var i =0;i<priority.length; i++){
-                //console.log(priority[i])
+            for(var i = 0;i<priority.length; i++){
                 if(priority[i]!= 0){
-                    return priority[i]
+                    return - priority[i]
                 }
             }
-
             return 0;
                 
         });
-
-
         var user = req.session.user
         return res.render('rank',{user,CLB})
     }
