64e5aaa53cd6a2c8427fcbd9d218d419287cf701
Vo Tu Trinh <votutrinh2002@gmail.com>
2022-12-20 20:42:19 +0700

    trinh so 1 phu my

--- controllers/RankController.js
+++ controllers/RankController.js
@@ -6,13 +6,15 @@ class RankController{
 
         var CLB = await Club.getAllClub();
 
+        console.log(CLB)
+
         const Rule = await Rules.getRulesFromDataBase();
 
         var priorityRank = Rule.priorityToRank;
 
-         CLB = await CLB.sort(async (a,b)=>{
+            CLB = await CLB.sort(async (a,b)=>{
             
-            const _score = a.score - b.score;
+            const _score = b.score - a.score;
             const _goalDelta = b.goalDelta - a.goalDelta;
             const _totalGoal = b.totalGoal - a.totalGoal;
             const _facing = await Match.getFacingOf2Club(a.name,b.name);
@@ -22,8 +24,9 @@ class RankController{
             const  priority = [ranks[priorityRank["p1"]],ranks[priorityRank["p2"]],ranks[priorityRank["p3"]],ranks[priorityRank["p4"]]]
 
             for(var i =0;i<priority.length; i++){
-                //console.log(priority[i])
+                console.log(priority[i])
                 if(priority[i]!= 0){
+
                     return priority[i]
                 }
             }
@@ -31,7 +34,6 @@ class RankController{
             return 0;
                 
         });
-
 
         var user = req.session.user
         return res.render('rank',{user,CLB})
