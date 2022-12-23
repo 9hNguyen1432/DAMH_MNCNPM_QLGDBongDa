ab3983abc473e71da45b1077f9b3769ba149cc00
Vo Tu Trinh <votutrinh2002@gmail.com>
2022-12-23 23:38:39 +0700

    sua loi dki

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -69,7 +69,7 @@ class authControllers{
         let errors =[];
         if(!email||!password||!fullname||!birthday||!gender||!role||!repass){
             errors.push("Vui lòng điền đầy đủ thông tin.")
-        if(password.length<6)
+        if(password.length < 6 )
             errors.push("Mật khẩu không được ít hơn 6 kí tự.")
         }
         if(password!=repass){
@@ -92,7 +92,7 @@ class authControllers{
                 }
                 return res.render('registerUser',{email,password,repass, fullname, birthday,gender,role,errors})
             }else{
-                const user =new User.constructor(uID,email,fullname,birthday,gender,role,fasle);
+                const user =new User.constructor(uID,email,fullname,birthday,gender,role,false);
                 await User.addUser(user);
                 return res.redirect('/auth/login');
             }
