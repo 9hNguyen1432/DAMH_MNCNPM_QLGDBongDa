5df3210e4441708f73a8f3b37ee457f31f87e9f7
Vo Tu Trinh <votutrinh2002@gmail.com>
2022-12-25 13:37:33 +0700

    update kq

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -39,7 +39,24 @@ class scheduleController{
 
         // AllMatchs = AllMatchs.reverse();
         var user = req.session.user
-        
+        AllMatchs = AllMatchs.sort((a,b)=>{
+            var dateParts1 = a.date.split("/");
+            var dateParts2 = b.date.split("/");
+            if(a.date==b.date){
+                var t1 = a.time.split(":");
+                var t2 = b.time.split(":");
+                var m1 = parseInt(t1[0]) * 60 + parseInt(t1[1]);
+                var m2 = parseInt(t2[0]) * 60 + parseInt(t2[1]);
+              return m1- m2
+            }
+    
+            return (
+             new Date(+dateParts1[2], dateParts1[1] - 1, +dateParts1[0])-
+               new Date(+dateParts2[2], dateParts2[1] - 1, +dateParts2[0])
+               
+            );
+        })
+
         res.render('danhsachtrandau',{user,AllMatchs ,date1: date[0], date2: date[1], date3: date[2] ,match1,match2, match3})
     }
     
